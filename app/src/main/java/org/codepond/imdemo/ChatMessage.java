f919f51c3cf36368042ec6832c549ae93d248c99
Nimrod Dayan <nimrod@codepond.org>
2017-03-13 14:56:39 +0200

    Created first view-presenter contract for chat feature

--- app/src/main/java/org/codepond/imdemo/ChatMessage.java
+++ app/src/main/java/org/codepond/imdemo/ChatMessage.java
@@ -3,11 +3,11 @@ package org.codepond.imdemo;
 import java.util.Date;
 
 public class ChatMessage {
-    String to;
-    String from;
-    String messageText;
-    boolean incomingMessage;
-    Date timestamp;
+    public String to;
+    public String from;
+    public String messageText;
+    public boolean incomingMessage;
+    public Date timestamp;
 
     public ChatMessage(String from, String to, String messageText, boolean remoteMessage, long timestamp) {
         this.from = from;
