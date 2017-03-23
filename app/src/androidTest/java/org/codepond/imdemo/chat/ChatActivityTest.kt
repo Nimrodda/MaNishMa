7893c35d6e025e7aa04e24cd885a9fe3ef1c99ef
Nimrod Dayan <nimrod@codepond.org>
2017-03-23 14:13:21 +0200

    Converted ChatActivity test to Kotlin and refactored to use robot pattern

---
+++ app/src/androidTest/java/org/codepond/imdemo/chat/ChatActivityTest.kt
@@ -0,0 +1,48 @@
+package org.codepond.imdemo.chat
+
+import android.content.Intent
+import android.support.test.rule.ActivityTestRule
+import android.support.test.rule.UiThreadTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.codepond.imdemo.ChatMessage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ChatActivityTest {
+    @Rule @JvmField
+    val mActivityTestRule: ActivityTestRule<ChatActivity> = object : ActivityTestRule<ChatActivity>(ChatActivity::class.java) {
+        override fun getActivityIntent(): Intent {
+            val intent = Intent()
+            intent.putExtra(ChatActivity.EXTRA_PARTICIPANT_JID, "user1@localhost/rLMACndayan")
+            return intent
+        }
+    }
+
+    @Rule @JvmField
+    val mUiThreadTestRule = UiThreadTestRule()
+
+    @Test
+    fun typeMessageAndClickSend_messageAddedToList() {
+        ChatRobot()
+                .typeMessage("Hello")
+                .clickSend()
+                .isShownOnRightSide()
+
+
+    }
+
+    @Test
+    fun receivedRemoteMessage_addedToList() {
+        val messageText = "Hello"
+        val chatMessage = ChatMessage("user2@localhost/rLMACndayan", "user1@localhost/rLMACndayan", messageText, true, System.currentTimeMillis())
+        val activity = mActivityTestRule.activity
+
+        mUiThreadTestRule.runOnUiThread { activity.mPresenter.onMessageReceived(chatMessage) }
+
+        ChatRobot()
+                .receiveMessage(messageText)
+                .isShownOnLeftSide()
+    }
+}
