5789d6249ac535f9397f9a3662ef8133d711582d
Nimrod Dayan <nimrod@codepond.org>
2017-04-01 21:47:16 +0300

    Migration to MVVM architecture: RecyclerView messages list converted to use data binding

--- app/src/main/java/org/codepond/imdemo/chat/ChatActivity.java
+++ app/src/main/java/org/codepond/imdemo/chat/ChatActivity.java
@@ -1,31 +1,30 @@
 package org.codepond.imdemo.chat;
 
 import android.content.ComponentName;
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableList;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import org.codepond.imdemo.BaseActivity;
-import org.codepond.imdemo.ChatMessage;
 import org.codepond.imdemo.R;
 import org.codepond.imdemo.databinding.ActivityChatBinding;
+import org.codepond.imdemo.databinding.MessageItemBinding;
 import org.codepond.imdemo.service.chat.ChatService;
 import org.codepond.imdemo.service.chat.MessagingService;
 
-import java.text.SimpleDateFormat;
-import java.util.List;
-
 public class ChatActivity extends BaseActivity {
     public static final String EXTRA_PARTICIPANT_JID = "extra_participant_jid";
-    private MessageAdapter mAdapter;
-    private RecyclerView mRecyclerView;
     ChatViewModel mChatViewModel;
 
     @Override
@@ -36,13 +35,11 @@ public class ChatActivity extends BaseActivity {
         mChatViewModel = new ChatViewModel(userJid, participantJid);
         ActivityChatBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_chat);
         binding.setModel(mChatViewModel);
-        mRecyclerView = (RecyclerView) findViewById(R.id.message_list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         layoutManager.setStackFromEnd(true);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mAdapter = new MessageAdapter();
-        mRecyclerView.setAdapter(mAdapter);
+        binding.messageList.setLayoutManager(layoutManager);
+        binding.messageList.setItemAnimator(new DefaultItemAnimator());
+        binding.messageList.setAdapter(new MessageAdapter(mChatViewModel.getMessages()));
         mChatViewModel.loadMessages();
     }
 
@@ -57,75 +54,93 @@ public class ChatActivity extends BaseActivity {
         mChatViewModel.stop();
     }
 
+    @BindingAdapter("remote")
+    public static void remote(View view, boolean isRemote) {
+        RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        if (isRemote) {
+            lp.addRule(RelativeLayout.ALIGN_PARENT_START, RelativeLayout.TRUE);
+        }
+        else {
+            lp.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE);
+        }
+    }
+
     private class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
-        private List<ChatMessage> mMessages;
+        @NonNull private ObservableArrayList<MessageViewModel> mMessages;
+
+        MessageAdapter(@NonNull ObservableArrayList<MessageViewModel> messages) {
+            mMessages = messages;
+        }
 
         @Override
         public MessageAdapter.MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            return new MessageViewHolder(inflater.inflate(R.layout.message_item, parent, false));
+            return new MessageViewHolder(MessageItemBinding.inflate(inflater, parent, false));
         }
 
         @Override
         public void onBindViewHolder(MessageAdapter.MessageViewHolder vh, int position) {
-            ChatMessage chatMessage = mMessages.get(position);
-            RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) vh.container.getLayoutParams();
-            if (chatMessage.getIncomingMessage()) {
-                if (isPreviousAuthorSame(position, chatMessage.getFrom())) {
-                    vh.container.setBackgroundResource(R.drawable.chat_bubble_incoming_ext);
-                }
-                else {
-                    vh.author.setText(chatMessage.getFrom());
-                    vh.author.setVisibility(android.view.View.VISIBLE);
-                    vh.container.setBackgroundResource(R.drawable.chat_bubble_incoming);
-
-                }
-                lp.addRule(RelativeLayout.ALIGN_PARENT_START, RelativeLayout.TRUE);
-            }
-            else {
-                if (isPreviousAuthorSame(position, chatMessage.getFrom())) {
-                    vh.container.setBackgroundResource(R.drawable.chat_bubble_outgoing_ext);
-                }
-                else {
-                    vh.container.setBackgroundResource(R.drawable.chat_bubble_outgoing);
-                }
-                lp.addRule(RelativeLayout.ALIGN_PARENT_END, RelativeLayout.TRUE);
-            }
-            vh.container.setLayoutParams(lp);
-            vh.messageItem.setText(chatMessage.getMessageText());
-            vh.timestamp.setText(SimpleDateFormat.getInstance().format(chatMessage.getTimestamp()));
-        }
-
-        private boolean isPreviousAuthorSame(int position, String displayName) {
-            return position > 0 && mMessages.get(position - 1).getFrom().equals(displayName);
+            MessageViewModel messageViewModel = mMessages.get(position);
+            vh.bind(messageViewModel, mChatViewModel);
         }
 
         @Override
         public int getItemCount() {
-            return mMessages != null ? mMessages.size() : 0;
+            return mMessages.size();
         }
 
-        void notifyNewMessageAdded() {
-            final int newMessagePosition = mMessages.size();
-            mAdapter.notifyItemInserted(newMessagePosition);
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+            mMessages.addOnListChangedCallback(mOnListChangedCallback);
         }
 
-        void setMessages(List<ChatMessage> chatMessages) {
-            mMessages = chatMessages;
-            notifyDataSetChanged();
+        @Override
+        public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
+            super.onDetachedFromRecyclerView(recyclerView);
+            mMessages.removeOnListChangedCallback(mOnListChangedCallback);
         }
+
+        private ObservableArrayList.OnListChangedCallback mOnListChangedCallback = new ObservableArrayList.OnListChangedCallback() {
+            @Override
+            public void onChanged(ObservableList observableList) {
+
+            }
+
+            @Override
+            public void onItemRangeChanged(ObservableList observableList, int i, int i1) {
+
+            }
+
+            @Override
+            public void onItemRangeInserted(ObservableList observableList, int i, int i1) {
+                final int newMessagePosition = mMessages.size();
+                notifyItemInserted(newMessagePosition);
+            }
+
+            @Override
+            public void onItemRangeMoved(ObservableList observableList, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onItemRangeRemoved(ObservableList observableList, int i, int i1) {
+
+            }
+        };
 
         class MessageViewHolder extends RecyclerView.ViewHolder {
-            TextView messageItem;
-            TextView timestamp;
-            TextView author;
-            android.view.View container;
-            MessageViewHolder(android.view.View view) {
-                super(view);
-                container = view.findViewById(R.id.message_container);
-                messageItem = (TextView) view.findViewById(R.id.message_item);
-                timestamp = (TextView) view.findViewById(R.id.timestamp);
-                author = (TextView) view.findViewById(R.id.author);
+            MessageItemBinding binding;
+            MessageViewHolder(MessageItemBinding messageItemBinding) {
+                super(messageItemBinding.getRoot());
+                binding = messageItemBinding;
+            }
+
+            void bind(MessageViewModel messageViewModel, ChatViewModel chatViewModel) {
+                binding.setMessage(messageViewModel);
+                binding.setChat(chatViewModel);
+                binding.setContext(getApplicationContext());
+                binding.executePendingBindings();
             }
         }
     }
